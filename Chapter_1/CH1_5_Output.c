e5ba365e65c36f87cc89f9e3f81f2d532b9839d8
MOYNUL75 <mmoynulislam2000@gmail.com>
2024-01-05 00:08:30 +0600

    Add Chapter 1 Output program

---
+++ Chapter_1/CH1_5_Output.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+    int age = 22;
+    float pi = 3.14;
+    char percentage = '%';
+
+    printf("Age is : %d\n", age);
+    printf("PI is : %f\n", pi);
+    printf("Character is : %c\n", percentage);
+    return 0;
+}
