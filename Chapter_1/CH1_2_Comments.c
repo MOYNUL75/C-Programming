55064f448d68377139c86f18201a395cdfffe216
MOYNUL75 <mmoynulislam2000@gmail.com>
2024-01-04 23:59:58 +0600

    Add Chapter 1 Comments program

---
+++ Chapter_1/CH1_2_Comments.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+    //This is Single line Comments.
+    printf("This is Single line Comments");
+    /*This 
+      is 
+      Multiple 
+      line 
+      Comments.
+    */
+    printf("This is Multiple line Comments");
+}
